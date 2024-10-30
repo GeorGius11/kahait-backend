b1099d7366d4d67dec5c45c9961289361a2a5dc0
George Rybko <george.rybko@gmail.com>
2024-10-30 23:51:10 +0200

    get answers query

--- src/answers/answers.resolver.ts
+++ src/answers/answers.resolver.ts
@@ -1,8 +1,9 @@
-import { Args, Int, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Info, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Answer } from './models/answer.model';
 import { AnswersService } from './answers.service';
 import { CreateAnswerInput } from './dto/create-answer.input';
 import { UpdateAnswerInput } from './dto/update-answer.input';
+import { GraphQLResolveInfo } from 'graphql';
 
 @Resolver(() => Answer)
 export class AnswersResolver {
@@ -12,18 +13,23 @@ export class AnswersResolver {
   async createAnswer(
     @Args('createAnswerInput') createAnswerInput: CreateAnswerInput,
   ) {
-    return this.answersService.createAnswer(createAnswerInput);
+    return this.answersService.create(createAnswerInput);
+  }
+
+  @Query(() => [Answer], { name: 'answers' })
+  async getAnswers(@Info() info?: GraphQLResolveInfo) {
+    return this.answersService.findAll(info);
   }
 
   @Mutation(() => Answer)
   async updateAnswer(
     @Args('updateAnswerInput') updateAnswerData: UpdateAnswerInput,
   ) {
-    return this.answersService.updateAnswer(updateAnswerData);
+    return this.answersService.update(updateAnswerData);
   }
 
   @Mutation(() => Answer)
   async deleteAnswer(@Args('id', { type: () => Int }) id: number) {
-    return this.answersService.deleteAnswer(id);
+    return this.answersService.delete(id);
   }
 }
--- src/answers/answers.service.ts
+++ src/answers/answers.service.ts
@@ -2,21 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateAnswerInput } from './dto/create-answer.input';
 import { UpdateAnswerInput } from './dto/update-answer.input';
+import { GraphQLResolveInfo } from 'graphql';
+import { PrismaSelectService } from 'src/prisma/prisma-select.service';
 
 @Injectable()
 export class AnswersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly prismaSelectService: PrismaSelectService,
+  ) {}
 
-  createAnswer(createAnswerInput: CreateAnswerInput) {
+  create(createAnswerInput: CreateAnswerInput) {
     return this.prisma.answer.create({ data: createAnswerInput });
   }
 
-  updateAnswer(updateAnswerData: UpdateAnswerInput) {
+  findAll(info?: GraphQLResolveInfo) {
+    const select = this.prismaSelectService.getValue(info);
+
+    return this.prisma.answer.findMany({
+      ...select,
+    });
+  }
+
+  update(updateAnswerData: UpdateAnswerInput) {
     const { id, ...data } = updateAnswerData;
     return this.prisma.answer.update({ where: { id }, data: data });
   }
 
-  deleteAnswer(id: number) {
+  delete(id: number) {
     return this.prisma.answer.delete({ where: { id } });
   }
 }
